375749876eba9f40db4c19288022b9dc9e1a6291
Steve Sebastian <sebastian@utexas.edu>
2016-01-19 14:23:33 -0600

    Experimental settings code
    Updated experimental settings code and added monitorDegreesToPixels.m

--- +nm/+experiment/loadExperimentSettings.m
+++ +nm/+experiment/loadExperimentSettings.m
@@ -5,7 +5,8 @@ function ExpSettings = loadExperimentSettings(ImgStats, expTypeStr, binIndex, cL
 
 if(strcmp(expTypeStr, 'fovea'))
     
-    monitorMaxPix = 255;
+    monitorMaxPix = 255;    
+    monitorSizePix = [1000 1000]; 
     
     filePathImages = ImgStats.Settings.filePathImages;
     targetTypeStr = 'gabor';
@@ -16,9 +17,13 @@ if(strcmp(expTypeStr, 'fovea'))
 	pTarget = 0.5;
 
 	pixelsPerDeg = 120;
+    
 	targetPosDeg = zeros(nLevels, nTrials, nBlocks, 2);
 	fixPosDeg = zeros(nLevels, nTrials, nBlocks, 2);
 
+    targetPosPix = nm.lib.monitorDegreesToPixels(targetPosDeg, monitorSizePix, pixelsPerDeg);
+    fixPosPix = nm.lib.monitorDegreesToPixels(targetPosDeg, monitorSizePix, pixelsPerDeg);
+    
 	targetFunction = @nm.lib.gabor2D;
 	loadStimuliFunction = @nm.experiment.additiveTarget;
 
@@ -28,5 +33,13 @@ if(strcmp(expTypeStr, 'fovea'))
         nm.experiment.samplePatchesForExperiment(ImgStats, tergetTypeStr, [5 5 5], filePathImages);
 	bgPixVal = ImgStats.Settings.binCenters.L(binIndex(1))*monitorMaxPix;
 
+    ExpSettings = cell('monitorMaxPix', monitorMaxPix, 'monitorSizePix', monitorSizePix, ...
+        'filePathImages', filePathImages, 'targetTypeStr', targetTypeStr, ...
+        'nLevels', nLevels, 'nTrials', nTrials, 'nBlocks', nBlocks, ...
+        'pTarget', pTarget, 'pixelsPerDeg', pixelsPerDeg, 'targetPosDeg', targetPosDeg, ...
+        'fixPosDeg', fixPosDeg, 'targetPosPix', targetPosPix, 'fixPosPix', fixPosPix, ...
+        'targetFunction', targetFunction, 'loadStimuliFunction', loadStimuliFunction, ...
+        'bTargetPresent', bTargetPresent, 'targeAmplitude', targetAmplitude, ...
+        'stimuli', stimuli, 'stimuliIndex', stimuliIndex, 'bgPixVal', bgPixVal);
 end
 
