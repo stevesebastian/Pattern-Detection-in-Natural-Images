050ef3751f915baf6c64e302b12e40d7deacadfb
Calen Walshe <calen.walshe@gmail.com>
2016-01-29 16:32:12 -0600

    Not finish integration yet. It's Friday, it's happy hour.

--- +experiment/+main/checkFixationCross.m
+++ +experiment/+main/checkFixationCross.m
@@ -7,26 +7,39 @@ if SessionSettings.el.dummyconnected % in dummy mode use mousecoordinates
     evt.geny = y;
     evtype=SessionSettings.el.ENDSACC;
 else % check for events
-    evtype=Eyelink('getnextdatatype');
-    evt = Eyelink('getfloatdata', evtype); % get data
+    evt = Eyelink('NewestFloatSample');
     
     t0 = GetSecs();
     maxTime = 5;
     fixRect = CenterRectOnPoint(SetRect(0,0,60,60), fixcrossPosX, fixcrossPosY);
     
     while 1        
-        if IsInRect(evt.genx, evt.geny, fixRect)
+        if IsInRect(evt.gx(2), evt.gy(2), fixRect)
             break;
-        elseif GetSecs() < t0 + maxTime
+        elseif GetSecs() > t0 + maxTime
             EyelinkDoTrackerSetup(SessionSettings.el);
+            
+            Screen('FillRect', SessionSettings.window, SessionSettings.bgPixVal);
+            
+            target   = SessionSettings.fixationTarget; 
+            
+            %% Redraw fixation cross and see how we do.
+
+            targetTexture      = Screen('Maketexture', SessionSettings.window, target);
+            targetRect         = SetRect(0, 0, size(target,2), size(target,1));
+            targetDestination  = floor(CenterRectOnPointd(targetRect, fixcrossPosX, fixcrossPosY)); 
+
+            
+            Screen('DrawTexture', SessionSettings.window, targetTexture, [], targetDestination);
+            Screen('Flip', SessionSettings.window, 0, 1);
+            
+            t0 = GetSecs();
         end
         
         [keyIsDown,secs, keyCode] = KbCheck;   
         if      keyCode(KbName('c'))
             EyelinkDoDriftCorrection(SessionSettings.el);
         end
-            
-        t = GetSecs();
         WaitSecs(.001);               
     end
     
