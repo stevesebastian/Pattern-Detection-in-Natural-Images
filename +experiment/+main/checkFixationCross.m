cd0628a4746091dfd3f2cf036c6f37a094662ce4
Calen Walshe <calen.walshe@gmail.com>
2016-02-01 16:16:10 -0600

    Integated and tested eyelink code on stimulus PC

--- +experiment/+main/checkFixationCross.m
+++ +experiment/+main/checkFixationCross.m
@@ -1,4 +1,4 @@
-function checkFixationCross(SessionSettings, fixcrossPosX, fixcrossPosY)
+function checkFixationCross(SessionSettings, fixPosPix)
 
 if SessionSettings.el.dummyconnected % in dummy mode use mousecoordinates
     [x,y,button] = GetMouse(SessionSettings.window);
@@ -11,25 +11,33 @@ else % check for events
     
     t0 = GetSecs();
     maxTime = 5;
-    fixRect = CenterRectOnPoint(SetRect(0,0,60,60), fixcrossPosX, fixcrossPosY);
+    fixRect = CenterRectOnPoint(SetRect(0,0,60,60), fixPosPix(1), fixPosPix(2));
     
-    while 1        
-        if IsInRect(evt.gx(2), evt.gy(2), fixRect)
+    while 1
+        if Eyelink('NewFloatSampleAvailable') > 0
+        	evt = Eyelink('NewestFloatSample');
+        end
+        
+        if IsInRect(evt.gx(1), evt.gy(1), fixRect)
             break;
         elseif GetSecs() > t0 + maxTime
-            EyelinkDoTrackerSetup(SessionSettings.el);
+            Eyelink('Command', 'set_idle_mode');
+            Eyelink('Command', 'clear_screen 0');
+            EyelinkDoDriftCorrection(SessionSettings.el);
+            Eyelink('Command', 'set_idle_mode');            
+            Eyelink('StartRecording');
+            % record a few samples before we actually start displaying
+            WaitSecs(0.1);
             
             Screen('FillRect', SessionSettings.window, SessionSettings.bgPixVal);
             
             target   = SessionSettings.fixationTarget; 
-            
             %% Redraw fixation cross and see how we do.
 
             targetTexture      = Screen('Maketexture', SessionSettings.window, target);
             targetRect         = SetRect(0, 0, size(target,2), size(target,1));
-            targetDestination  = floor(CenterRectOnPointd(targetRect, fixcrossPosX, fixcrossPosY)); 
+            targetDestination  = floor(CenterRectOnPointd(targetRect, fixPosPix(1), fixPosPix(2))); 
 
-            
             Screen('DrawTexture', SessionSettings.window, targetTexture, [], targetDestination);
             Screen('Flip', SessionSettings.window, 0, 1);
             
@@ -38,7 +46,22 @@ else % check for events
         
         [keyIsDown,secs, keyCode] = KbCheck;   
         if      keyCode(KbName('c'))
-            EyelinkDoDriftCorrection(SessionSettings.el);
+            Eyelink('Command', 'set_idle_mode');
+            Eyelink('Command', 'clear_screen 0');
+            EyelinkDoTrackerSetup(SessionSettings.el,'c');
+            Eyelink('Command', 'set_idle_mode');            
+            Eyelink('StartRecording');
+            target   = SessionSettings.fixationTarget; 
+            %% Redraw fixation cross and see how we do.
+
+            targetTexture      = Screen('Maketexture', SessionSettings.window, target);
+            targetRect         = SetRect(0, 0, size(target,2), size(target,1));
+            targetDestination  = floor(CenterRectOnPointd(targetRect, fixPosPix(1), fixPosPix(2))); 
+
+            Screen('DrawTexture', SessionSettings.window, targetTexture, [], targetDestination);
+            Screen('Flip', SessionSettings.window, 0, 1);
+            t0 = GetSecs()
+            WaitSecs(0.1);
         end
         WaitSecs(.001);               
     end
