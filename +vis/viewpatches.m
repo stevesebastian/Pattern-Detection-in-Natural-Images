4aaea93739ffc29cf071a3163fb1c2d53c198113
Calen Walshe <calen.walshe@gmail.com>
2016-02-05 14:59:38 -0600

    Added visualization scripts.
    Two scripts were added. A visualization package was also created.

---
+++ +vis/viewpatches.m
@@ -0,0 +1,48 @@
+function viewpatches(ImgStats, L,C,S, targetNr, bText, bSave, imgDir)
+% VIEWPATCHES Step through patches in a bin.
+%   Description: Step through one image at a time.
+% 
+%   v1 1/28/2016 R.C.Walshe (calen.walshe@utexas.edu)
+
+if nargin == 1
+    L = 8;
+    C = 8;
+    S = 1;
+    targetNr    = 1;
+    bText       = 1;
+    bSave       = 0;
+    imgDir      = '~/occluding/natural_images/pixel_space/';
+end
+
+[target, win] = haar2D();
+
+patchList = ImgStats.patchIndex{targetNr,1}(L,C,S);
+try 
+    randPatch = randsample(patchList{1}, 100);
+catch
+    error('Error in acquiring patches');
+end
+    
+[patchNr, imgNr] = ind2sub(size(ImgStats.L), randPatch);
+
+h = figure('Position', [100, 100, 21, 21]);
+
+for i = 1:length(patchNr)
+
+    I_PPM = load([ImgStats.Settings.imgFilePath, ImgStats.imgDir(imgNr(i)).name]);
+    I     = I_PPM.I_PPM;
+    
+    cropped = lib.cropImage(I, ImgStats.sampleCoords(patchNr(i),:), 21,1,1).*win;
+    
+    colormap(gray(2^14)); imagesc(cropped);
+
+    if bText
+        fprintf('L:%f\n',round(ImgStats.L(patchNr(i), imgNr(i)),2));
+        fprintf('C:%f\n',round(ImgStats.C(patchNr(i), imgNr(i)),2)); 
+        fprintf('S:%f\n\n',round(ImgStats.Sa(patchNr(i), imgNr(i),targetNr),2));
+    end
+    axis image;
+    pause(.25);            
+end
+
+end
