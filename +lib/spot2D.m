bb6c75944d539f36e27cf30bd584eec2a96bdc56
Calen Walshe <calen.walshe@gmail.com>
2016-02-16 11:59:59 -0600

    Merge branch 'master' into peripheral-pilot
    # Conflicts:
    #	+lib/spot2D.m

--- +lib/spot2D.m
+++ +lib/spot2D.m
@@ -1,17 +1,28 @@
-function spot = spot(spotParams)
+function spot = spot2D(stimulusParams, tWin)
+% SPOT2D Create a pedestal stimulus. /----\
+%                                   |  __  | 
+%                                   | |__| |
+%                                    \____/                 
+% R. Calen Walshe 02/12/2016 (calen.walshe@utexas.edu)
 
+paramNames      =  {'pixperdeg','size','dc','contrast','type'};
+param_fields    = fieldnames(stimulusParams);
+has_params      = ismember(paramNames,param_fields);
+if(any(~has_params))
+    error('Poorly specified haar parameter set');
+end    
 
-
-spotRadPx     = floor(spotParams.spotWidth/2);
-ppd           = spotParams.ppd;
-widthIntDeg    = spotRadPx/ppd * .75;
+spotRadPx      = floor((stimulusParams.size*stimulusParams.pixperdeg)/2);
+widthIntDeg    = spotRadPx/stimulusParams.pixperdeg * .75;
 
 [XX, YY] = meshgrid(-spotRadPx:spotRadPx);
 
-dGrid = sqrt(XX.^2 + YY.^2) ./ ppd;
+dGrid = sqrt(XX.^2 + YY.^2) ./ stimulusParams.pixperdeg;
 
 spot = zeros(size(dGrid));
 
 spot(dGrid > widthIntDeg) = -1;
 spot(dGrid < widthIntDeg) = 1;
+
+spot = (spot - mean(spot(tWin(:)))) .* tWin
 end
