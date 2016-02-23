f7827c11373400f4ed2496664cce49659daf2d55
Calen Walshe <calen.walshe@gmail.com>
2016-02-23 17:43:01 -0600

    Spot stimulus changed to have equal area in the internal and external region of the stimulus.

--- +lib/spot2D.m
+++ +lib/spot2D.m
@@ -1,9 +1,17 @@
-function spot = spot2D(stimulusParams, tWin)
+function spot = spot2D(stimulusParams)
 % SPOT2D Create a pedestal stimulus. /----\
 %                                   |  __  | 
 %                                   | |__| |
 %                                    \____/                 
 % R. Calen Walshe 02/12/2016 (calen.walshe@utexas.edu)
+
+if nargin < 1
+    stimulusParams.pixperdeg = 60;
+    stimulusParams.size        = .35;
+    stimulusParams.dc          = 0;    
+    stimulusParams.contrast    = 1;
+    stimulusParams.type        = 'spot';
+end
 
 paramNames      =  {'pixperdeg','size','dc','contrast','type'};
 param_fields    = fieldnames(stimulusParams);
@@ -12,18 +20,19 @@ if(any(~has_params))
     error('Poorly specified haar parameter set');
 end    
 
-spotRadPx      = floor((stimulusParams.size*stimulusParams.pixperdeg)/2);
-widthIntDeg    = spotRadPx/stimulusParams.pixperdeg * .75;
+spotRadPx       = floor((stimulusParams.size/2)*stimulusParams.pixperdeg);
+
+interiorRadDeg  = stimulusParams.size/2 * 1/sqrt(2);
 
 [XX, YY] = meshgrid(-spotRadPx:spotRadPx);
 
-dGrid = sqrt(XX.^2 + YY.^2) ./ stimulusParams.pixperdeg;
+dGrid       = sqrt(XX.^2 + YY.^2) ./ stimulusParams.pixperdeg;
+envelope    = dGrid < stimulusParams.size/2;
 
-spot = zeros(size(dGrid));
+spot = ones(size(dGrid)) * -1;
 
-spot(dGrid > widthIntDeg) = -1;
-spot(dGrid < widthIntDeg) = 1;
+spot(dGrid < interiorRadDeg) = 1;
 
-spot = (spot - mean(spot(tWin(:)))) .* tWin;
+spot = (spot - mean(spot(envelope(:)))) .* envelope;
 
 end
